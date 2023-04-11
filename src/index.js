30a186e1da4173688e2d261486d35adf8e7eb18f
Lucas Nunes <120993347+thezunes@users.noreply.github.com>
2023-04-11 04:13:52 -0300

    Post Tweet

--- src/index.js
+++ src/index.js
@@ -12,12 +12,31 @@ app.use(express.json())
 app.post("/sign-up", (request, response) => {
    const {username, avatar} = request.body;
   
-    if (!username || !avatar) { response.status(422).send("Você precisa preencher todos os campos")}
+    if (!username || !avatar) { response.status(422).send("[ERRO] Você precisa preencher todos os campos")}
     else {
     const user = {username, avatar};
     usersList.push(user);
-    response.status(201).send("ok");
+    console.log("success")
+    response.status(201).send("OK");
+    console.log(usersList)
     }
   }); 
+    
+
+    app.post("/tweets", (request, response) => {
+        const { username, tweet } = request.body    
+        const registred = usersList.some(user => user.username === username)
+
+        if (registred)
+        {
+            const newTweet = { username, tweet };
+            tweets.push(newTweet);
+            console.log("success");
+            response.status(201).send("OK");
+        } else {
+            console.log("UNAUTHORIZED");
+            response.status(401).send("UNAUTHORIZED");
+        }
+    });
 
 app.listen(5000, () => console.log("servidor rodando na porta 5000"))
