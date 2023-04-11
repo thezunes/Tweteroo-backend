1e8b44c87e0f9e3e88cb3d66d72051be9e04f376
Lucas Nunes <120993347+thezunes@users.noreply.github.com>
2023-04-11 02:55:51 -0300

    Post Sign-up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import express from "express"
+import cors from "cors"
 
 const app = express();
 
-app.get("/teste", (resquest, response) => {
+const usersList = [];
+const tweets = [];
 
-response.send("testeee")
+app.use(cors())
+app.use(express.json())
 
+app.post("/sign-up", (request, response) => {
+
+const {username, avatar} = request.body
+
+if(!username || !avatar){ return response.status(422).send("Você precisa preencher todos os campos");}
+
+const user = {username, avatar};
+usersList.push(user);
+return response.status(201).send("ok");
 
 } )
 
-
-app.listen(5000, () => console.log("servidor rodando"))
+app.listen(5000, () => console.log("servidor rodando na porta 5000"))
